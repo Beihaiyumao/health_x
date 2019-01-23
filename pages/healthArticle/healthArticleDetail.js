288390c5164e931b176cec6ccf07cc77832f1feb
Gujing <xiaoyc@example.com>
2019-01-24 00:14:03 +0800

    新增文章评论
    新增文章评论

--- pages/healthArticle/healthArticleDetail.js
+++ pages/healthArticle/healthArticleDetail.js
@@ -15,9 +15,29 @@ Page({
     createTime: '',
     "comment_show": true,
 
-    "collectinfo": ''
+    "collectinfo": '',
+    inputComment:false,//是否显示评论框
+    comment:'',//评论内容
+    // 最大字符数
+    maxTextLen: 50,
+    // 默认长度
+    textLen: 0,
+    userId:"",
   },
+  //获取用户输入内容
+  comment(e) {
+    let page = this;
+    // 设置最大字符串长度(为-1时,则不限制)
+    let maxTextLen = page.data.maxTextLen;
+    // 文本长度
+    let textLen = e.detail.value.length;
 
+    page.setData({
+      maxTextLen: maxTextLen,
+      textLen: textLen,
+      comment: e.detail.value,
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -38,6 +58,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
+    console.log(this.data.articleId)
     var that = this;
     //获取文章具体信息
     wx.request({
@@ -127,5 +148,65 @@ onReachBottom: function() {
  */
 onShareAppMessage: function() {
 
-}
+},
+/**
+ * 点击评论
+ */
+  commentBtnClick: function(){
+    var that =this;
+    that.setData({
+      inputComment:true,
+    })
+  },
+/**
+ * 发布评论
+ */
+  releaseComment:function(){
+    var that = this;
+    var isrightful = that.checkInput();
+    console.log(isrightful)
+    if (isrightful){
+      wx.request({
+        url: urlPath + '/healthyArticle/insertArticleComment',
+        method: 'POST',
+        data: ({
+          articleId: this.data.articleId,
+          content: this.data.comment,
+          userId: wx.getStorageSync('userId'),
+        }),
+        success: function (e) {
+          console.log(e);
+          if (e.data.code == 100) {
+            wx.showToast({
+              title: '评论成功', //这里成功
+              icon: 'success',
+              duration: 1000,
+            });
+          }
+          else {
+            wx.showLoading({
+              title: '发布失败',
+              duration: 1500,
+            })
+          }
+        }
+      })
+    }
+    
+    
+  },
+  checkInput:function(){
+    console.log(this.data.comment+"kog")
+    var commentIsK =/^(?!(\.s+$))/;
+    if(this.data.comment==null || this.data.comment==""|| this.data.comment==undefined || commentIsK.test(this.data.comment)){
+      wx.showLoading({
+        title: '请输入正确内容',
+        duration:1500,
+      })
+      return false;
+    }else{
+      return true;
+    }
+    
+  }
 })
